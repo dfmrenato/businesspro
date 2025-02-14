5aabf36a7cec90ae637bd45733d7655d7e5997a9
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-02-14 18:41:51 -0300

    25.31

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
 function pesquisar() {
-    const form = document.getElementById('cabecalhopesquisa');
-    const pesq = form.elements['pesquisa'];
-    alert(`Você pesquisou: ${pesq.value}`);
+    alert()
 }
