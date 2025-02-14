f2fdff70bbf6040b3de8fdd8159fd6c6b712be5c
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-02-14 18:25:44 -0300

    Melhor pesquisa

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-document.getElementById('jsteste').innerHTML = "OI!"
+document.getElementById('cabecalho-pesquisa').addEventListener('submit', (event) => {
+    event.preventDefault();
+    const form = document.getElementById('cabecalho-pesquisa');
+    const pesquisa = form.elements['pesquisa']; // accessing element by name
+    window.alert(`Você pesquisou: ${pesquisa}`)
+})
