7bd6607dd13813482e3e1212c52f72490d218cf0
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-02-14 18:43:02 -0300

    Pesquisa (Agora vai)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-function pesquisar() {
-    alert()
-}
+document.getElementById('registroForm').addEventListener('submit', function(event) {
+    event.preventDefault();  // Previne o envio real do formulário
+
+    // Obtém os valores dos campos do formulário
+    const pesquisa = document.getElementById('pesquisa').value;
+
+    // Exibe um alerta com os dados preenchidos
+    alert(`Dados do Formulário:
+    Pesquisa: ${pesquisa}`);
+});
