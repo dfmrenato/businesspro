4fec00662308d42f24e25b6712e0f954d351187c
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-02-14 18:44:23 -0300

    Pesquisar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.getElementById('registroForm').addEventListener('submit', function(event) {
+document.getElementById('cabecalhopesquisa').addEventListener('submit', function(event) {
     event.preventDefault();  // Previne o envio real do formulário
 
     // Obtém os valores dos campos do formulário
