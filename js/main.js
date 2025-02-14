d8b7d14449c753a03d09021fb30d06f298984ebb
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-02-14 18:50:48 -0300

    Base para pesquisa implementada

--- js/main.js
+++ js/main.js
@@ -2,11 +2,10 @@ document.getElementById('cabecalhopesquisa').addEventListener('submit', (event) 
     event.preventDefault();  // Previne o envio real do formulário
 
     // Obtém os valores dos campos do formulário
-    const pesquisa = document.getElementById('pesquisa').value;
+    const pesquisa = document.getElementById('cabecalhopesquisa').elements["pesquisa"].value;
 
     // Exibe um alerta com os dados preenchidos
-    alert(`Dados do Formulário:
-    Pesquisa: ${pesquisa}`);
+    window.alert(`Pesquisa: ${pesquisa}`);
 });
 
 function verificar_internet() {
