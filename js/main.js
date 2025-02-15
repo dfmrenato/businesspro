34911a8a5ba9e0249f49c304a89855d0adb2bc8e
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-02-15 13:34:14 -0300

    logon.js

--- js/main.js
+++ js/main.js
@@ -22,17 +22,3 @@ document.getElementById('MenuIcone').addEventListener('click', () => {
         document.querySelector('.MenuIcone img').src = "img/icone/fechar.png";
     };
 })
-
-// Abrir o menu de login ou registro
-document.getElementById('LoginOpcao').addEventListener('click', () => {
-    document.querySelector('#LoginOpcao').classList.add('ativo');
-    document.querySelector('#LoginMenu').classList.add('ativo');
-    document.querySelector('#RegistroOpcao').classList.remove('ativo');
-    document.querySelector('#RegistroMenu').classList.remove('ativo');
-});
-document.getElementById('RegistroOpcao').addEventListener('click', () => {
-    document.querySelector('#LoginOpcao').classList.remove('ativo');
-    document.querySelector('#LoginMenu').classList.remove('ativo');
-    document.querySelector('#RegistroOpcao').classList.add('ativo');
-    document.querySelector('#RegistroMenu').classList.add('ativo');
-});
