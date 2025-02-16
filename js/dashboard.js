50d24ae7e2aec299ce31e24442d826bb06272248
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-02-16 20:24:45 +0000

    Bolo esfriando

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,3 +1,12 @@
+// Página acessível apenas para usuários logados
 if(!sessionStorage.getItem('UsuarioLogado')) {
-    location.replace('./logon');
+    location.replace('./index');
+}
+
+// Selecionar uma aba
+const Abas = document.getElementsByClassName('BarraLateralItem');
+for (const aba in Abas) {
+    aba.addEventListener('click', () => {
+        Notificar('Título', 'Você foi moggado');
+    });
 }
