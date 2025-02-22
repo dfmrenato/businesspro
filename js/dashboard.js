f71b1e8376ed67cd87f5af8008ea9228ba2909e0
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-02-22 17:38:24 +0000

    Correções <br> e aba padrão

--- js/dashboard.js
+++ js/dashboard.js
@@ -26,3 +26,53 @@ Abas.forEach(aba => {
 
     });
 });
+
+// Aba pelo link
+let AbaPadrao = Abas.find(aba => window.location.hash.replace("#","").startsWith(aba.children.namedItem('span').id.replace("LateralAba","")));
+if(AbaPadrao) {
+    let aba = AbaPadrao;
+    aba.classList.add('ativo');
+    CorpoAbas.find(caba => caba.id.replace("Corpo","Lateral") == aba.children.namedItem('span').id).classList.add('ativo');
+} else {
+    let aba = Abas.find(aba => aba.children.namedItem('span').id == "LateralAbaInicio");
+    aba.classList.add('ativo');
+    CorpoAbas.find(caba => caba.id.replace("Corpo","Lateral") == aba.children.namedItem('span').id).classList.add('ativo');
+}
+
+// Pesquisar funcionários
+document.getElementById('FuncionariosCabecalhoPesquisa').addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const empresa = window.sessionStorage.getItem('UsuarioLogadoEmpresa');
+    const pesquisa = document.getElementById('FuncionariosCabecalhoPesquisa').elements["pesquisa"].value;
+    const classificar = document.getElementById('FuncionariosCabecalhoPesquisa').elements["classificar"].value;
+
+    try {
+        // Comunicação com o backend
+        const response = await fetch('https://evolved-legible-spider.ngrok-free.app/dashboard-funcionarios-pesquisa', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ empresa, pesquisa, classificar })
+        });
+
+        const data = await response.json();
+
+        if(data.error_message) return Notificar(`Erro de pesquisa`, `${data.error_message}`, 'OK');
+
+        if (!response.ok) {
+            throw new Error('Falha na solicitação');
+        }
+
+        // Código específico
+        
+
+    } catch (error) {
+        console.error(error);
+        Notificar('Erro de pesquisa', error, 'OK');
+    }
+
+})
+
+// Pegar funcionários
