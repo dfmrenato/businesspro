5fd3df4ab0407ec3268673c8cf1b53a453a88cdf
Renato Augusto <renato.santos36@aluno.educa.go.gov.br>
2025-03-02 18:01:45 +0000

    Pontinho da sorte

--- js/service_worker.js
+++ js/service_worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'offline-cache-v1';
-const OFFLINE_URL = '/offline.html';
+const OFFLINE_URL = './offline.html';
 
 self.addEventListener('install', async (event) => {
     event.waitUntil(
