8cf3f084cf8d0bdbd97ab81b6e968c290ae9f07c
Renato <renato.augusto.almeida.santos@gmail.com>
2025-08-10 00:07:32 +0000

    Exportação Android (e teoricamente iOS)

---
+++ mobile/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View, StatusBar } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="dark-content" />
+      <WebView source={{ uri: 'https://businesspro.vercel.app' }} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
