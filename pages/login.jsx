bf4abfcea1ce6d9c335b772cd74e393b6248678e
Renato <renato.augusto.almeida.santos@gmail.com>
2025-08-10 18:17:34 +0000

    Visual do cabeçalho aprimorado

--- pages/login.jsx
+++ pages/login.jsx
@@ -19,7 +19,57 @@ export default function Index() {
       </Head>
       <Header />
       <main>
-        Corpo
+        <div className="LogonSeletor">
+          <button className="Opcao ativo" id="RegistroOpcao">cadastrar nova conta</button>
+          <button className="Opcao" id="LoginOpcao">entrar em conta existente</button>
+        </div>
+
+        <div className="Logon">
+
+          <div id="RegistroMenu" className="LogOption ativo">
+            <h1>Criar uma nova conta</h1>
+            <form id="RegistroFormulario">
+              <label htmlFor="NomeR">Nome</label><br />
+              <input className="FormularioCampoPrimario" type="text" id="NomeR" name="nome" placeholder="Ovídio Antoninho Marques" required /><br />
+
+              <label htmlFor="EmpresaR">Empresa</label><br />
+              <input className="FormularioCampoPrimario" type="text" id="EmpresaR" name="empresa" placeholder="Nome da empresa" required /><br />
+
+              <label htmlFor="EmailR">E-mail</label><br />
+              <input className="FormularioCampoPrimario" type="email" id="EmailR" name="email" placeholder="fulano@email.com" required /><br />
+
+              <label htmlFor="SenhaR">Senha</label><br />
+              <input className="FormularioCampoPrimario" type="password" id="SenhaR" name="senha" placeholder="****" required />
+              <button className="ExibirSenha" id="ExibirSenhaR" type="button"><i className="fa-solid fa-eye"></i></button><br />
+
+              <input type="hidden" name="tipo" value="empresarial" />
+
+              <div className="LogOptionBotoes">
+                <button className="BotaoTerciario" type="reset">Limpar dados</button>
+                <button className="BotaoPrimario" type="submit">Realizar cadastro</button>
+              </div>
+            </form>
+          </div>
+
+          <div id="LoginMenu" className="LogOption">
+            <h1>Entrar na sua conta</h1>
+            <form id="LoginFormulario">
+              <label htmlFor="EmailL">E-mail</label><br />
+              <input className="FormularioCampoPrimario" type="email" id="EmailL" name="email" placeholder="fulano@email.com" required /><br />
+
+              <label htmlFor="SenhaL">Senha</label><br />
+              <input className="FormularioCampoPrimario" type="password" id="SenhaL" name="senha" placeholder="****" required />
+              <button className="ExibirSenha" id="ExibirSenhaL" type="button"><i className="fa-solid fa-eye"></i></button><br />
+
+              <input type="hidden" name="tipo" value="login" />
+              <div className="LogOptionBotoes">
+                <button className="BotaoTerciario" id="BotaoEsqueciMinhaSenha" type="button">Esqueci minha senha</button>
+                <button className="BotaoPrimario" type="submit">Entrar</button>
+              </div>
+            </form>
+          </div>
+
+        </div>
       </main>
     </>
   );
